587642fe8d4126061087e3b3b2f91b2d6f029ea0
incogiscool <adam546944@gmail.com>
2022-11-30 21:29:53 +0400

    fix button ui

--- components/barricade/MainButton.tsx
+++ components/barricade/MainButton.tsx
@@ -7,11 +7,13 @@ type MainButtonType = {
 
 const MainButton = ({ text, link }: MainButtonType) => {
   return (
-    <Link href={link}>
-      <button className="sm:m-10 m-5 font-medium bg-buttonBackground hover:bg-buttonBackgroundAlt text-main w-[150px] h-[35px] border border-main rounded-2xl transition">
-        {text}
-      </button>
-    </Link>
+    <div className="sm:m-10 m-5">
+      <Link href={link}>
+        <button className="font-medium bg-buttonBackground hover:bg-buttonBackgroundAlt text-main w-[150px] h-[35px] border border-main rounded-2xl transition">
+          {text}
+        </button>
+      </Link>
+    </div>
   );
 };
 

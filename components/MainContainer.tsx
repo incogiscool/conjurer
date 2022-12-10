029671d6402392ef7f72e13bac9d9848cae525f0
incogiscool <adam546944@gmail.com>
2022-12-10 10:26:47 +0400

    enable mobile support because not work

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -10,19 +10,19 @@ type MainContainerTypes = {
 const MainContainer = ({ children, title, className }: MainContainerTypes) => {
   const [supported, setSupported] = useState(true);
 
-  useState(() => {
-    if (
-      navigator.userAgent.match(/Android/i) ||
-      navigator.userAgent.match(/webOS/i) ||
-      navigator.userAgent.match(/iPhone/i) ||
-      navigator.userAgent.match(/iPad/i) ||
-      navigator.userAgent.match(/iPod/i) ||
-      navigator.userAgent.match(/BlackBerry/i) ||
-      navigator.userAgent.match(/Windows Phone/i)
-    ) {
-      setSupported(false);
-    }
-  }, []);
+  // useState(() => {
+  //   if (
+  //     navigator.userAgent.match(/Android/i) ||
+  //     navigator.userAgent.match(/webOS/i) ||
+  //     navigator.userAgent.match(/iPhone/i) ||
+  //     navigator.userAgent.match(/iPad/i) ||
+  //     navigator.userAgent.match(/iPod/i) ||
+  //     navigator.userAgent.match(/BlackBerry/i) ||
+  //     navigator.userAgent.match(/Windows Phone/i)
+  //   ) {
+  //     setSupported(false);
+  //   }
+  // }, [navigator.userAgent]);
 
   return (
     <div className={className}>

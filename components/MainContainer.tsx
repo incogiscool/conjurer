7c9994f5f2b9b77fbcb68e38af95adaeaca5e29d
incogiscool <adam546944@gmail.com>
2022-12-10 10:12:15 +0400

    disabled mobile support

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 
 type MainContainerTypes = {
   children: ReactElement;
@@ -8,12 +8,28 @@ type MainContainerTypes = {
 };
 
 const MainContainer = ({ children, title, className }: MainContainerTypes) => {
+  const [supported, setSupported] = useState(true);
+
+  useState(() => {
+    if (
+      navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/webOS/i) ||
+      navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/iPod/i) ||
+      navigator.userAgent.match(/BlackBerry/i) ||
+      navigator.userAgent.match(/Windows Phone/i)
+    ) {
+      setSupported(false);
+    }
+  });
+
   return (
     <div className={className}>
       <Head>
         <title>{title}</title>
       </Head>
-      {children}
+      {supported ? <div>{children}</div> : <div>device not supported</div>}
     </div>
   );
 };

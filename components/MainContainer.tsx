545e75bd19d43b57b90a7cd4f6df3f306f0b001b
incogiscool <94598096+incogiscool@users.noreply.github.com>
2022-12-12 08:32:02 +0200

    deleted comment, added variable on useEffect

--- components/MainContainer.tsx
+++ components/MainContainer.tsx
@@ -11,25 +11,13 @@ const MainContainer = ({ children, title, className }: MainContainerTypes) => {
   const [onMobile, setOnMobile] = useState(true);
 
   let isOnMobile;
+
   useEffect(() => {
     isOnMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     if (isOnMobile) {
       setOnMobile(true);
     } else setOnMobile(false);
-  }, []);
-  // useState(() => {
-  //   if (
-  //     navigator.userAgent.match(/Android/i) ||
-  //     navigator.userAgent.match(/webOS/i) ||
-  //     navigator.userAgent.match(/iPhone/i) ||
-  //     navigator.userAgent.match(/iPad/i) ||
-  //     navigator.userAgent.match(/iPod/i) ||
-  //     navigator.userAgent.match(/BlackBerry/i) ||
-  //     navigator.userAgent.match(/Windows Phone/i)
-  //   ) {
-  //     setSupported(false);
-  //   }
-  // }, [navigator.userAgent]);
+  }, [isOnMobile]);
 
   return (
     <div className={className}>

4429f42711d0a4323c716eba3d691666f2d883bc
incogiscool <94598096+incogiscool@users.noreply.github.com>
2022-12-19 13:40:00 +0200

    change to mainnet-beta

--- utils/barricade-js/constants.ts
+++ utils/barricade-js/constants.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const connectionCluster: string =
-  "https://flashy-proportionate-breeze.solana-mainnet.discover.quiknode.pro/49db30a0b4eb3f8d07e88d844e9a24ddbdf86dc9/";
+export const connectionCluster: string = "https://api.mainnet-beta.solana.com";
+// "https://flashy-proportionate-breeze.solana-mainnet.discover.quiknode.pro/49db30a0b4eb3f8d07e88d844e9a24ddbdf86dc9/";
 export const lockNFTFee: number = 0.01;
 export const barricadeTitle: string = "Barricade - Keeping your tokens secure.";
 export const barricadeFeeWallet: PublicKey = new PublicKey(

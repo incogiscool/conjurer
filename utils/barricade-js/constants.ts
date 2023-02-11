c465e913b5acebefeea997b678119baa362654f5
Adam El Taha <94598096+incogiscool@users.noreply.github.com>
2023-02-11 23:27:58 +0400

    change rpc

--- utils/barricade-js/constants.ts
+++ utils/barricade-js/constants.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const connectionCluster: string =
-  "https://try-rpc.mainnet.solana.blockdaemon.tech/";
+export const connectionCluster: string = "https://virulent-fittest-sheet.solana-mainnet.discover.quiknode.pro/245dc550c480096e6722b1e5557ca9d0aa62f9dd/";
+  //"https://try-rpc.mainnet.solana.blockdaemon.tech/";
 // "https://neat-thrilling-mound.solana-devnet.discover.quiknode.pro/3683bc7fbdae1bbb0aea6658e8f4c1cba43a65fb/"; //devnet
 export const lockNFTFee: number = 0.01;
 export const barricadeTitle: string = "Barricade - Keeping your tokens secure.";

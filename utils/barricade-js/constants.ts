ab14518597fcf1bc0d3de156966096c4d027f603
Adam El Taha <94598096+incogiscool@users.noreply.github.com>
2023-04-13 05:44:24 +0400

    update node

--- utils/barricade-js/constants.ts
+++ utils/barricade-js/constants.ts
@@ -1,7 +1,8 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const connectionCluster: string =
-  "https://virulent-fittest-sheet.solana-mainnet.discover.quiknode.pro/245dc550c480096e6722b1e5557ca9d0aa62f9dd/";
+      "https://bitter-cool-pool.solana-mainnet.discover.quiknode.pro/39a8516c321a2052ea8fa965125b5f30e9088729/"
+//   "https://virulent-fittest-sheet.solana-mainnet.discover.quiknode.pro/245dc550c480096e6722b1e5557ca9d0aa62f9dd/";
 // "https://neat-thrilling-mound.solana-devnet.discover.quiknode.pro/3683bc7fbdae1bbb0aea6658e8f4c1cba43a65fb/";
 // "https://try-rpc.mainnet.solana.blockdaemon.tech/";
 // "https://flashy-proportionate-breeze.solana-mainnet.discover.quiknode.pro/49db30a0b4eb3f8d07e88d844e9a24ddbdf86dc9/";
